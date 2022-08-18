8939e7f7ee91061498671b8de28fd36528430d6f
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2022-08-19 01:38:08 +0800

    Use BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
