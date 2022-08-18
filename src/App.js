0b1f05b818960a6d7600756d5f559501bfa1e92a
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2022-08-18 23:28:18 +0800

    Fix the uri for the apollo client

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const cache = new InMemoryCache({
 
 
 const client = new ApolloClient({
-  uri: 'https://proyekto-app.herokuapp.com',
+  uri: 'https://proyekto-app.herokuapp.com/graphql',
   cache
 })
 
