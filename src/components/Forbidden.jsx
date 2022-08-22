2218b8b8f5c8021a60a56b6f1676da709898a65b
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2022-08-22 08:38:54 +0800

    Change the forbidden code to 403

--- src/components/Forbidden.jsx
+++ src/components/Forbidden.jsx
@@ -6,7 +6,7 @@ export default function Forbidden() {
     <>
     <div className="d-flex flex-column justify-content-center align-items-center mt-5">
         <AiOutlineStop className="text-danger" size='5em' />
-        <h1>404</h1>
+        <h1>403</h1>
         <p className="Lead">Sorry, you don't have the permission to access this page.</p>
         <Link to="/login" className="btn btn-primary">
             login
